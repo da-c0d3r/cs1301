cb20f74e69d2c91002b9c3fea808adaebd9dfcbc
Daniel Nikolaev <danielnik2016@gmail.com>
2024-07-23 14:58:37 -0400

    5.1 fuckerssss

--- Unit_5/5.1 Problem Set/5.1.15-adv.py
+++ Unit_5/5.1 Problem Set/5.1.15-adv.py
@@ -86,109 +86,114 @@ class Beans:
             
 #Add and modify your code here!
 class Burrito:
-    def __init__(self, meat, to_go, rice, beans, extra_meat = False, guacamole = False, cheese = False, pico = False, corn = False):
-        self.meat = self.set_meat(meat)
-        self.to_go = self.set_to_go(to_go)
-        self.rice = self.set_rice(rice)
-        self.beans = self.set_beans(beans)
-        self.extra_meat = self.set_extra_meat(extra_meat)
-        self.guacamole = self.set_guacamole(guacamole)
-        self.cheese = self.set_cheese(cheese)
-        self.pico = self.set_pico(pico)
-        self.corn = self.set_corn(corn)
+    def __init__(self, meat, to_go, rice, beans, extra_meat=False, guacamole=False, cheese=False, pico=False, corn=False):
+        self.meat = Meat(meat)
+        self.rice = Rice(rice)
+        self.beans = Beans(beans)
+        self.set_to_go(to_go)
+        self.set_extra_meat(extra_meat)
+        self.set_guacamole(guacamole)
+        self.set_cheese(cheese)
+        self.set_pico(pico)
+        self.set_corn(corn)
 
-    # setters
-    def set_meat(self, meat):
-        valid = ["chicken", "pork", "steak", "tofu"]
-        if meat in valid:
-            self.meat = meat
-        else:
-            self.meat = False
-        return self.meat
-    def set_to_go(self, to_go):
-        if type(to_go) != bool:
-            self.to_go = False
-        else:
-            self.to_go = to_go
-        return self.to_go
-    def set_rice(self, rice):
-        valid = ["brown", "white"]
-        if rice in valid:
-            self.rice = rice
-        else:
-            self.rice = False
-        return self.rice
-    def set_beans(self, beans):
-        valid = ["black", "pinto"]  
-        if beans in valid:
-            self.beans = beans
-        else:
-            self.beans = False
-        return self.beans
-    def set_extra_meat(self, extra_meat):
-        if type(extra_meat) != bool:
-            self.extra_meat = False
-        else:
-            self.extra_meat = extra_meat
-        return self.extra_meat
-    def set_guacamole(self, guacamole): 
-        if type(guacamole) != bool:
-            self.guacamole = False
-        else:
-            self.guacamole = guacamole
-        return self.guacamole
-    def set_cheese(self, cheese):
-        if type(cheese) != bool:
-            self.cheese = False
-        else:
-            self.cheese = cheese
-        return self.cheese
-    def set_pico(self, pico):    
-        if type(pico) != bool:
-            self.pico = False
-        else:
-            self.pico = pico     
-        return self.pico      
-    def set_corn(self, corn):
-        if type(corn) != bool:
-            self.corn = False
-        else:
-            self.corn = corn
-        return self.corn
-
-    # getters
+# getters
     def get_meat(self):
-        return self.meat
+        return self.meat.get_value()
+
     def get_to_go(self):
         return self.to_go
+
     def get_rice(self):
-        return self.rice
+        return self.rice.get_value()
+
     def get_beans(self):
-        return self.beans
+        return self.beans.get_value()
+
     def get_extra_meat(self):
         return self.extra_meat
+
     def get_guacamole(self):
         return self.guacamole
+
     def get_cheese(self):
         return self.cheese
+
     def get_pico(self):
         return self.pico
+
     def get_corn(self):
         return self.corn
 
+
+# settters
+    def set_meat(self, meat):
+        return self.meat.set_value(meat)
+
+    def set_to_go(self, to_go):
+        if type(to_go) == bool:
+            self.to_go = to_go
+        else:
+            self.to_go = False
+        return self.to_go
+
+    def set_rice(self, rice):
+        return self.rice.set_value(rice)
+
+    def set_beans(self, beans):
+        return self.beans.set_value(beans)
+
+    def set_extra_meat(self, extra_meat):
+        if type(extra_meat) == bool:
+            self.extra_meat = extra_meat
+        else:
+            self.extra_meat = False
+        return self.extra_meat
+
+    def set_guacamole(self, guacamole):
+        if type(guacamole) == bool:
+            self.guacamole = guacamole
+        else:
+            self.guacamole = False
+        return self.guacamole
+
+    def set_cheese(self, cheese):
+        if type(cheese) == bool:
+            self.cheese = cheese
+        else:
+            self.cheese = False
+        return self.cheese
+
+    def set_pico(self, pico):
+        if type(pico) == bool:
+            self.pico = pico
+        else:
+            self.pico = False
+        return self.pico
+
+    def set_corn(self, corn):
+        if type(corn) == bool:
+            self.corn = corn
+        else:
+            self.corn = False
+        return self.corn
+
+
     def get_cost(self):
-        cost = 5.0
-        meater = ["chicken", "pork", "tofu"]
-        
-        if self.meat in meater:
-            cost += 1.00
-        elif self.meat == "steak":
-            cost += 1.50
-        if (self.extra_meat == True) and (self.meat != False):    
-            cost += 1.00
-        if self.guacamole:
-            cost += 0.75
-        return cost
+        self.price = 5.00
+
+        if self.meat.get_value() in ["chicken", "pork", "tofu"]:
+            self.price += 1.00
+        if self.meat.get_value() == "steak":
+            self.price += 1.50
+        if self.extra_meat == True and self.meat.get_value() != False:
+            self.price += 1.00
+        if self.guacamole == True:
+            self.price += 0.75
+
+        return self.price
+
+
 
 
 #Below are some lines of code that will test your class.
